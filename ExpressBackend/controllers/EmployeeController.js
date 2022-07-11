d760644e5beef92b52d70c9c929d654aa4bd2c0a
ICARUS-2 <ebriffett01@gmail.com>
2022-07-11 12:35:39 -0400

    Update method works

--- ExpressBackend/controllers/EmployeeController.js
+++ ExpressBackend/controllers/EmployeeController.js
@@ -64,7 +64,7 @@ router.post('/new', async (req, res) =>
 
         employee.save()
         res.statusCode = 201;
-        res.send(employee)
+        res.send("Employee successfully created")
     }
     catch(err)
     {
@@ -74,9 +74,62 @@ router.post('/new', async (req, res) =>
 })
 
 //Update existing employee
-router.post('/edit', async (req, res) =>
+router.post('/:id/edit', async (req, res) =>
 {
-    //TODO
+    let id = req.params.id.toString();
+
+    let emp;
+
+    //First try to find the employee
+    try
+    {
+        emp = await EmployeeModel.find().where({"_id": id})
+
+        if (emp.length != 1)
+        {
+            res.statusCode = 404
+            res.send("No employee with ID "+id+ " exists")
+            return;
+        }
+    }
+    catch(err)
+    {
+        res.statusCode = 404
+        res.send("No employee with ID "+id+ " exists")
+        return;
+    }
+
+    //Then update it once its existence is confirmed
+    try
+    {
+        let name = req.body.name;
+        let dob = req.body.dateOfBirth;
+        let email = req.body.email;
+        let hiringDate = req.body.hiringDate;
+        let terminationDate = req.body.terminationDate;
+        let status = req.body.status;
+        let info = req.body.info;
+
+        emp[0].name = name;
+        emp[0].dateOfBirth = dob;
+        emp[0].email = email;
+        emp[0].hiringDate = hiringDate;
+        emp[0].terminationDate = terminationDate;
+        emp[0].status = status;
+        emp[0].info = info;
+
+        emp[0].save();
+        res.statusCode = 201
+        res.send("Employee " + id + " successfully edited")
+        return;
+    }
+    catch(err)
+    {
+        console.log(err)
+        res.statusCode = 400
+        res.send(err);
+        return;
+    }
 })
 
 //Remove employee
