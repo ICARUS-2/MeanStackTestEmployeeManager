da8ac2782a9d851c67f3825dc8f46140e8d54338
ICARUS-2 <ebriffett01@gmail.com>
2022-07-11 16:53:26 -0400

    Added create page

--- EmployeeFrontend/src/app/app-routing.module.ts
+++ EmployeeFrontend/src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EmployeesListComponent } from './employees-list/employees-list.component';
+import { CreateEmployeeComponent } from './create-employee/create-employee.component';
 
 const routes: Routes = [
-  {path: "", component: EmployeesListComponent}
+  {path: "", component: EmployeesListComponent},
+  {path: "new", component: CreateEmployeeComponent}
 ];
 
 @NgModule({
--- EmployeeFrontend/src/models/EmployeeModel.ts
+++ EmployeeFrontend/src/models/EmployeeModel.ts
@@ -3,17 +3,19 @@ export default class EmployeeModel
     _id: string = "";
     name: string = "";
     email: string = "";
+    department: string = ""
     dateOfBirth: Date | null = null;
     hiringDate: Date | null = null;
     terminationDate: Date | null = null;
     status: string = "";
     info: string = "";
 
-    constructor(id: string, name: string, email: string, dateOfBirth: Date | null, hiringDate: Date | null, terminationDate: Date | null, status: string, info: string)
+    constructor(id: string, name: string, email: string, department: string, dateOfBirth: Date | null, hiringDate: Date | null, terminationDate: Date | null, status: string, info: string)
     {
         this._id = id;
         this.name = name;
         this.email = email;
+        this.department = department;
 
         if (dateOfBirth != null)
             this.dateOfBirth = new Date(dateOfBirth);
